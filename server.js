3aa874bf64e8f39817b5485913ff282bc9c5b1b1
Jessica <64239329+jessillaw@users.noreply.github.com>
2021-12-18 20:49:23 -0800

    Express Simple REST

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.delete('/campsites', (req, res) => {
     res.end('Deleting all campsites');
 });
 
-app.get('campsites/:campsiteId', (req,res) => {
+app.get('/campsites/:campsiteId', (req,res) => {
     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 });
 
@@ -43,7 +43,7 @@ app.post('/campsites/:campsiteId',(req, res) => {
 
 app.put('/campsites/:campsiteId', (req, res) => {
     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite ${req.body.description}`);
+    res.end(`Will update the campsite ${req.body.name} with description: ${req.body.description}`);
 });
 
 app.delete('/campsites/:campsiteId', (req, res) => {
